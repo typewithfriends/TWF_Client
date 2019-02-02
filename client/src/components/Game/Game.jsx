3c291c9debbfd1c083b31997c86a9397b3c069dc
Tommy Liao <tommyliao340@hotmail.com>
2019-02-01 19:15:30 -0800

    i hate life

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,40 +1,39 @@
-import './game.css';
-import React from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
-import { getPrompt, getCurrentLetter, updateProgress } from '../../actions/index.js';
+  import './mainapp.css';
+  import React from 'react';
+  import { connect } from 'react-redux';
+  import axios from 'axios';
+  import { getPrompt, getCurrentLetter, updateProgress } from '../../actions/index.js';
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
+  class Game extends React.Component {
+    constructor(props) {
+      super(props);
 
-    this.getKey = this.getKey.bind(this);
-  }
-
-  componentDidMount() {
-    axios.get('/api/prompt')
-      .then(({ data }) => {
-        data = data.quotes[0].quote;
-        this.props.getPrompt(data);
-      })
-      .catch(err => console.error(err, 'unable to get prompt'));
-
-    this.props.getKeyFn(this.getKey);
-  }
-
-  getKey(e) {
-    if (e.key === this.props.prompt[this.props.currentLetter]) {
-      let updatedProgress = (this.props.currentLetter + 1) / this.props.prompt.length;
-      this.props.getCurrentLetter(this.props.currentLetter + 1);
-      this.props.updateProgress(updatedProgress);
+      this.getKey = this.getKey.bind(this);
     }
-    if (this.props.currentLetter === this.props.prompt.length) {
-      console.log('done');
-      // maybe emit winner here
-    }
-  }
 
-  
+    componentDidMount() {
+      axios.get('/api/prompt')
+        .then(({ data }) => {
+          data = data.quotes[0].quote;
+          this.props.getPrompt(data);
+        })
+        .catch(err => console.error(err, 'unable to get prompt'));
+
+      this.props.getKeyFn(this.getKey);
+    }
+
+    getKey(e) {
+      if (e.key === this.props.prompt[this.props.currentLetter]) {
+        let updatedProgress = (this.props.currentLetter + 1) / this.props.prompt.length;
+        this.props.getCurrentLetter(this.props.currentLetter + 1);
+        this.props.updateProgress(updatedProgress);
+      }
+      if (this.props.currentLetter === this.props.prompt.length) {
+        console.log('done');
+        // maybe emit winner here
+      }
+    }
+
     render() {
       return (
         <div onClick={this.props.getGameFocus} className="game">
@@ -78,21 +77,19 @@ class Game extends React.Component {
         </div>
       );
     }
-}
-
-const mapStateToProps = state => {
-  console.log(state)
-  return {
-    prompt: state.prompt,
-    currentLetter: state.currentLetter,
-    progress: state.progress
   }
-}
 
-export default connect(mapStateToProps, {
-  getPrompt,
-  getCurrentLetter,
-  updateProgress
-})(Game);
+  const mapStateToProps = state => {
+    console.log(state)
+    return {
+      prompt: state.prompt,
+      currentLetter: state.currentLetter,
+      progress: state.progress
+    }
+  }
 
-// 
+  export default connect(mapStateToProps, {
+    getPrompt,
+    getCurrentLetter,
+    updateProgress
+  })(Game);
